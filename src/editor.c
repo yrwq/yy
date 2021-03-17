b1931052a423d7ab6d4f6296873c2e5f958f9046
yrwq <yrwqid@gmail.com>
2021-03-17 15:16:08 +0100

    Add a function to get terminal size

--- src/editor.c
+++ src/editor.c
@@ -1,7 +1,6 @@
-#include <errno.h>
-#include <libgen.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 #include <term/input.h>
 #include <term/mode.h>
@@ -9,14 +8,35 @@
 
 editor_state_t editor = {};
 
+void draw_rows() {
+    int y;
+    for (y = 0; y < 24; y++) {
+        printf("~\r\n");
+    }
+    set_cursor(0, 0);
+}
+
+
 void insert_char(char c) {
     printf("%c", c);
     fflush(stdout);
 }
 
+int get_term_size(int * rows, int * cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 void yy_init() {
     term_clear();
     set_cursor(0, 0);
+    draw_rows();
     editor.running = 1;
 }
 
@@ -25,3 +45,4 @@ void yy_quit() {
     /* fclose(editor.file); */
     exit(1);
 }
+
