6de0474ca628b0a1085ef59b7267d7684ec6d135
yrwq <yrwqid@gmail.com>
2021-03-17 15:21:26 +0100

    Draw tildes

--- src/editor.c
+++ src/editor.c
@@ -33,6 +33,24 @@ int get_term_size(int * rows, int * cols) {
     }
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+
+    buf[i] = '\0';
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
 void yy_init() {
     get_term_size(&editor.rows, &editor.cols);
     term_clear();
